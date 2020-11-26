ab6ab78dfc9a8fc933a90a0c8f12c083a4aa3913
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-11-26 12:15:26 +0100

    initial setup for a rascal language server

---
+++ rascal-lsp/src/main/java/org/rascalmpl/RascalLanguageServer.java
@@ -0,0 +1,10 @@
+package org.rascalmpl;
+
+import org.eclipse.lsp4j.services.LanguageServer;
+/**
+ * The main language server class for Rascal is build on top of the Eclipse
+ * lsp4j library
+ */
+public class RascalLanguageServer implements LanguageServer {
+
+}
