490d94e3b94b0abad529c4ac72520d425928ea2d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-11-27 12:06:45 +0100

    added vscode extension code from template using -yo code-

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalLanguageServer.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalLanguageServer.java
@@ -1,10 +1,22 @@
 package org.rascalmpl.vscode.lsp;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
+import org.eclipse.lsp4j.CompletionOptions;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.InitializedParams;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+import org.eclipse.lsp4j.launch.LSPLauncher;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
 import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import org.eclipse.lsp4j.services.WorkspaceService;
@@ -13,12 +25,21 @@ import org.eclipse.lsp4j.services.WorkspaceService;
  * The main language server class for Rascal is build on top of the Eclipse
  * lsp4j library
  */
-public class RascalLanguageServer implements LanguageServer {
+public class RascalLanguageServer implements LanguageServer, LanguageClientAware {
+
+    private static final RascalTextDocumentService RASCAL_TEXT_DOCUMENT_SERVICE = new RascalTextDocumentService();
+    private static final RascalWorkspaceService RASCAL_WORKSPACE_SERVICE = new RascalWorkspaceService();
+	private int errorCode = 1;
+	private LanguageClient client;
 
     @Override
     public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
-        // TODO Auto-generated method stub
-        return null;
+        final InitializeResult initializeResult = new InitializeResult(new ServerCapabilities());
+
+        initializeResult.getCapabilities().setTextDocumentSync(TextDocumentSyncKind.Full);
+        CompletionOptions completionOptions = new CompletionOptions();
+        initializeResult.getCapabilities().setCompletionProvider(completionOptions);
+        return CompletableFuture.supplyAsync(()->initializeResult);
     }
 
     @Override
@@ -29,22 +50,43 @@ public class RascalLanguageServer implements LanguageServer {
 
     @Override
     public CompletableFuture<Object> shutdown() {
-        // TODO Auto-generated method stub
+        this.errorCode = 0;
         return null;
     }
 
     @Override
     public void exit() {
-        // TODO Auto-generated method stub
+        System.exit(errorCode);
     }
 
     @Override
     public TextDocumentService getTextDocumentService() {
-        return new RascalTextDocumentService();
+        return RASCAL_TEXT_DOCUMENT_SERVICE;
     }
 
     @Override
     public WorkspaceService getWorkspaceService() {
-        return new RascalWorkspaceService();
+        return RASCAL_WORKSPACE_SERVICE;
+    }
+
+    @Override
+    public void connect(LanguageClient client) {
+        this.client = client;
+    }
+
+    public static void main(String[] args) {
+        LogManager.getLogManager().reset();
+        Logger globalLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        globalLogger.setLevel(Level.OFF);
+        
+        RascalLanguageServer server = new RascalLanguageServer() ;
+        Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
+        server.connect(launcher.getRemoteProxy());
+        Future<Void> startListening = launcher.startListening();
+        try {
+			startListening.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
     }
 }
--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalTextDocumentService.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalTextDocumentService.java
@@ -1,34 +1,148 @@
 package org.rascalmpl.vscode.lsp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.lsp4j.CodeLens;
+import org.eclipse.lsp4j.CodeLensParams;
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionItemKind;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.DefinitionParams;
 import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.DocumentFormattingParams;
+import org.eclipse.lsp4j.DocumentHighlight;
+import org.eclipse.lsp4j.DocumentHighlightParams;
+import org.eclipse.lsp4j.DocumentOnTypeFormattingParams;
+import org.eclipse.lsp4j.DocumentRangeFormattingParams;
+import org.eclipse.lsp4j.Hover;
+import org.eclipse.lsp4j.HoverParams;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.LocationLink;
+import org.eclipse.lsp4j.ReferenceParams;
+import org.eclipse.lsp4j.RenameParams;
+import org.eclipse.lsp4j.SignatureHelp;
+import org.eclipse.lsp4j.SignatureHelpParams;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.WorkspaceEdit;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.TextDocumentService;
 
 public class RascalTextDocumentService implements TextDocumentService {
 
     @Override
-    public void didOpen(DidOpenTextDocumentParams params) {
-        // TODO Auto-generated method stub
+    public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams completionParams) {
+        // Provide completion item.
+        return CompletableFuture.supplyAsync(() -> {
+            List<CompletionItem> completionItems = new ArrayList<>();
+            try {
+                // Sample Completion item for sayHello
+                CompletionItem completionItem = new CompletionItem();
+                // Define the text to be inserted in to the file if the completion item is selected.
+                completionItem.setInsertText("import IO;\n\nvoid main(list[str] args) {\n    println(args);\n}\n");
+                // Set the label that shows when the completion drop down appears in the Editor.
+                completionItem.setLabel("example main");
+                // Set the completion kind. This is a snippet.
+                // That means it replace character which trigger the completion and
+                // replace it with what defined in inserted text.
+                completionItem.setKind(CompletionItemKind.Snippet);
+                // This will set the details for the snippet code which will help user to
+                // understand what this completion item is.
+                completionItem.setDetail("main; this is an example Rascal main function");
+
+                // Add the sample completion item to the list.
+                completionItems.add(completionItem);
+            } catch (Exception e) {
+                // handle exception
+            }
+
+            // Return the list of completion items.
+            return Either.forLeft(completionItems);
+        });
+    }
+
+    @Override
+    public CompletableFuture<CompletionItem> resolveCompletionItem(CompletionItem completionItem) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Hover> hover(HoverParams params) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<SignatureHelp> signatureHelp(SignatureHelpParams params) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> definition(DefinitionParams params) {
+        return TextDocumentService.super.definition(params);
+    }
+
+    @Override
+    public CompletableFuture<List<? extends Location>> references(ReferenceParams referenceParams) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<List<? extends DocumentHighlight>> documentHighlight(DocumentHighlightParams params) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<List<? extends CodeLens>> codeLens(CodeLensParams codeLensParams) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<CodeLens> resolveCodeLens(CodeLens codeLens) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<List<? extends TextEdit>> formatting(DocumentFormattingParams documentFormattingParams) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<List<? extends TextEdit>> rangeFormatting(DocumentRangeFormattingParams documentRangeFormattingParams) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<List<? extends TextEdit>> onTypeFormatting(DocumentOnTypeFormattingParams documentOnTypeFormattingParams) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<WorkspaceEdit> rename(RenameParams renameParams) {
+        return null;
+    }
+
+    @Override
+    public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
 
     }
 
     @Override
-    public void didChange(DidChangeTextDocumentParams params) {
-        // TODO Auto-generated method stub
+    public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
 
     }
 
     @Override
-    public void didClose(DidCloseTextDocumentParams params) {
-        // TODO Auto-generated method stub
+    public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
 
     }
 
     @Override
-    public void didSave(DidSaveTextDocumentParams params) {
-        // TODO Auto-generated method stub
+    public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
 
     }
 
