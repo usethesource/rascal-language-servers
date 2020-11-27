da7916f46d9aeeab3c56df2b180c1c91df8e2f60
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2020-11-27 12:23:57 +0100

    added client stuff

--- rascal-vscode-extension/src/extension.ts
+++ rascal-vscode-extension/src/extension.ts
@@ -1,26 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+const main: string = 'RascalLanguageServer';
+const version: string = '1.0.0';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rascal-mpl" is now active!');
+	console.log('Rascal MPL extension is active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rascal-mpl.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// Get the java home from the process environment.
+	const { JAVA_HOME } = process.env;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Rascal Metaprogramming Language!');
-	});
+	console.log(`Using java from JAVA_HOME: ${JAVA_HOME}`);
+	// If java home is available continue.
+	if (JAVA_HOME) {
+		// Java execution path.
+		let excecutable: string = path.join(JAVA_HOME, 'bin', 'java');
 
-	context.subscriptions.push(disposable);
+		// path to the launcher.jar
+		let classPath = path.join(__dirname, '..', 'launcher', 'rascal-vscode-extension-' + version + '.jar');
+		const args: string[] = ['-cp', classPath];
+
+		// Set the server options 
+		// -- java execution path
+		// -- argument to be pass when executing the java command
+		let serverOptions: ServerOptions = {
+			command: excecutable,
+			args: [...args, main],
+			options: {}
+		};
+
+		// Options to control the language client
+		let clientOptions: LanguageClientOptions = {
+			// Register the server for plain text documents
+			documentSelector: [{ scheme: 'file', language: 'rascalmpl' }]
+		};
+
+		// Create the language client and start the client.
+		let disposable = new LanguageClient('rascal-mpl', 'Rascal MPL Language Server', serverOptions, clientOptions).start();
+
+		// Disposables to remove on deactivation.
+		context.subscriptions.push(disposable);
+	}
 }
 
 // this method is called when your extension is deactivated
