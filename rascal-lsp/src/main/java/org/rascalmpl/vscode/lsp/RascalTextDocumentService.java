6258a9a6047e73882465c55b88dd17699fde85d0
Davy Landman <davy.landman@swat.engineering>
2021-02-23 21:50:17 +0100

    Added outline support

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalTextDocumentService.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalTextDocumentService.java
@@ -12,10 +12,10 @@
  */
 package org.rascalmpl.vscode.lsp;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -24,11 +24,9 @@ import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 import org.eclipse.lsp4j.DefinitionParams;
@@ -37,12 +35,16 @@ import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.DocumentSymbol;
+import org.eclipse.lsp4j.DocumentSymbolParams;
 import org.eclipse.lsp4j.Location;
 import org.eclipse.lsp4j.LocationLink;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.PublishDiagnosticsParams;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.SymbolInformation;
+import org.eclipse.lsp4j.SymbolKind;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.eclipse.lsp4j.TextDocumentItem;
 import org.eclipse.lsp4j.TextDocumentSyncKind;
@@ -53,14 +55,11 @@ import org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageClientAware;
 import org.eclipse.lsp4j.services.TextDocumentService;
-import org.rascalmpl.interpreter.NullRascalMonitor;
-import org.rascalmpl.library.util.PathConfig;
 import org.rascalmpl.parser.gtd.exception.ParseError;
 import org.rascalmpl.uri.URIUtil;
 import org.rascalmpl.values.IRascalValueFactory;
 import org.rascalmpl.values.parsetrees.ITree;
 import org.rascalmpl.values.parsetrees.TreeAdapter;
-import org.rascalmpl.vscode.lsp.model.Summary;
 import org.rascalmpl.vscode.lsp.util.Diagnostics;
 import org.rascalmpl.vscode.lsp.util.ErrorReporter;
 import org.rascalmpl.vscode.lsp.util.FileState;
@@ -68,8 +67,13 @@ import org.rascalmpl.vscode.lsp.util.FileState;
 import io.usethesource.vallang.ICollection;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IList;
+import io.usethesource.vallang.IMap;
+import io.usethesource.vallang.INode;
 import io.usethesource.vallang.ISet;
 import io.usethesource.vallang.ISourceLocation;
+import io.usethesource.vallang.IString;
+import io.usethesource.vallang.IValue;
+import io.usethesource.vallang.IWithKeywordParameters;
 
 public class RascalTextDocumentService implements TextDocumentService, LanguageClientAware, ErrorReporter {
 	private final ExecutorService ownExcecutor = Executors.newCachedThreadPool();
@@ -88,6 +92,7 @@ public class RascalTextDocumentService implements TextDocumentService, LanguageC
 	public void initializeServerCapabilities(ServerCapabilities result) {
 		result.setDefinitionProvider(true);
 		result.setTextDocumentSync(TextDocumentSyncKind.Full);
+        result.setDocumentSymbolProvider(true);
 	}
 
 	@Override
@@ -105,10 +110,8 @@ public class RascalTextDocumentService implements TextDocumentService, LanguageC
 
 	@Override
 	public void report(ICollection<?> msgs) {
-		appendDiagnostics(
-			msgs.stream()
-			.map(d -> (IConstructor) d)
-			.map(d -> new SimpleEntry<>(((ISourceLocation) d.get("at")).top(), Diagnostics.translateDiagnostic(d))));
+        appendDiagnostics(msgs.stream().map(d -> (IConstructor) d).map(
+                d -> new SimpleEntry<>(((ISourceLocation) d.get("at")).top(), Diagnostics.translateDiagnostic(d))));
 	}
 
 	@Override
@@ -144,11 +147,13 @@ public class RascalTextDocumentService implements TextDocumentService, LanguageC
 		// 	try {
 		// 		IList files = vf.list(src);
 		// 		PathConfig pcfg = PathConfig.fromSourceProjectMemberRascalManifest(src);
-		// 		IList messages = rascalServices.compileFileList(new NullRascalMonitor(), files, pcfg);
+        // IList messages = rascalServices.compileFileList(new NullRascalMonitor(),
+        // files, pcfg);
 		// 		report(messages);
 		// 	} catch (IOException e) {
 		// 		Logger.getGlobal().log(Level.INFO,
-		// 				"compilation/typechecking of " + params.getTextDocument().getUri() + " failed", e);
+        // "compilation/typechecking of " + params.getTextDocument().getUri() + "
+        // failed", e);
 		// 	}
 		// });
 
@@ -164,24 +169,27 @@ public class RascalTextDocumentService implements TextDocumentService, LanguageC
 
 	@Override
 	public void didSave(DidSaveTextDocumentParams params) {
-		// TODO didSave never happens. Perhaps because file sync is on FULL instead of INCREMENTAL?
+        // TODO didSave never happens. Perhaps because file sync is on FULL instead of
+        // INCREMENTAL?
 		// try {
-		// 	Summary summary = getFile(toLoc(params.getTextDocument())).getSummary().get();
+        // Summary summary =
+        // getFile(toLoc(params.getTextDocument())).getSummary().get();
 		// 	report(summary.getMessages());
 		// } catch (InterruptedException | ExecutionException e) {
-		// 	Logger.getGlobal().log(Level.INFO, "compilation/typechecking of " + params.getTextDocument().getUri() + " failed", e);
+        // Logger.getGlobal().log(Level.INFO, "compilation/typechecking of " +
+        // params.getTextDocument().getUri() + " failed", e);
 		// }
 	}
 
 	@Override
-	public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> definition(DefinitionParams params) {
+    public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> definition(
+            DefinitionParams params) {
 		FileState file = getFile(toLoc(params.getTextDocument()));
 
 		final int column = params.getPosition().getCharacter();
 		final int line = params.getPosition().getLine();
 
-		return file.getSummary()
-			.thenApply(s -> {
+        return file.getSummary().thenApply(s -> {
 				final ITree tree = file.getCurrentTree();
 				ITree lexical = TreeAdapter.locateLexical(tree, line, column);
 
@@ -190,13 +198,74 @@ public class RascalTextDocumentService implements TextDocumentService, LanguageC
 				}
 
 				return toLSPLocation(s.definition(TreeAdapter.getLocation(lexical)));
-			})
-			.thenApply(l -> locList(l))
-			.exceptionally(e -> locList())
-			;
+        }).thenApply(l -> locList(l)).exceptionally(e -> locList());
+    }
+
+    @Override
+    public CompletableFuture<List<Either<SymbolInformation, DocumentSymbol>>> documentSymbol(
+            DocumentSymbolParams params) {
+        return getFile(toLoc(params.getTextDocument())).getCurrentTreeAsync().thenApply(rascalServices::getOutline)
+                .thenApply(RascalTextDocumentService::buildOutlineTree);
 	}
 
 	// Private utility methods
+
+    private static List<Either<SymbolInformation, DocumentSymbol>> buildOutlineTree(INode outline) {
+
+        List<Either<SymbolInformation, DocumentSymbol>> result = new ArrayList<>();
+        for (IValue g : outline) {
+            INode group = (INode) g;
+            SymbolKind kind = translateKind(group.getName());
+            if (kind == null) {
+                continue;
+            }
+            IValue arg = group.get(0);
+            if (arg instanceof IList) {
+                for (IValue e : (IList) arg) {
+                    result.add(buildOutlineEntry(kind, (INode) e));
+                }
+            } else if (arg instanceof IMap) {
+                ((IMap)arg).valueIterator().forEachRemaining(v -> {
+                    for (IValue e: (IList)v) {
+                        result.add(buildOutlineEntry(kind, (INode) e));
+                    }
+                });
+            }
+        }
+        return result;
+    }
+
+    private static Either<SymbolInformation, DocumentSymbol> buildOutlineEntry( SymbolKind kind, INode element) {
+        IWithKeywordParameters<? extends IValue> kwParams = element.asWithKeywordParameters();
+        ISourceLocation loc = (ISourceLocation) kwParams.getParameter("loc");
+        if (loc == null) {
+            loc =  URIUtil.invalidLocation();
+        }
+        Range target = loc != null ? toRange(loc) : new Range(new Position(0,0), new Position(0,0));
+        IString details = (IString) kwParams.getParameter("label");
+        DocumentSymbol result;
+        if (details == null) {
+            result = new DocumentSymbol(element.getName(), kind, target, target);
+        }
+        else {
+            result = new DocumentSymbol(element.getName(), kind, target, target, details.getValue());
+        }
+        return Either.forRight(result);
+    }
+
+    private static SymbolKind translateKind(String name) {
+        switch (name) {
+            case "Functions": return SymbolKind.Function;
+            case "Tests": return SymbolKind.Method;
+            case "Variables": return SymbolKind.Variable;
+            case "Aliases": return SymbolKind.Class;
+            case "Data": return SymbolKind.Struct;
+            case "Tags": return SymbolKind.Property;
+            case "Imports": return null;
+            case "Syntax": return SymbolKind.Interface;
+        }
+        return null;
+    }
 
 	private static Either<List<? extends Location>, List<? extends LocationLink>> locList(Location... l) {
 		return Either.<List<? extends Location>, List<? extends LocationLink>>forLeft(Arrays.asList(l));
