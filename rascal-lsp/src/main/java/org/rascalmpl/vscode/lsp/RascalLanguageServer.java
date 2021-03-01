a1c86fd5eef97ea0b5b8152bb568e6115735fd19
Davy Landman <davy.landman@swat.engineering>
2021-03-01 11:49:44 +0100

    Splitup the server

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalLanguageServer.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/RascalLanguageServer.java
@@ -10,13 +10,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.ServerCapabilities;
@@ -30,57 +25,18 @@ import org.eclipse.lsp4j.services.WorkspaceService;
 /**
  * The main language server class for Rascal is build on top of the Eclipse lsp4j library
  */
-public class RascalLanguageServer implements LanguageServer, LanguageClientAware {
+public class RascalLanguageServer {
     private static final Logger logger = LogManager.getLogger(RascalLanguageServer.class);
 
-    private static final RascalTextDocumentService RASCAL_TEXT_DOCUMENT_SERVICE = new RascalTextDocumentService();
-    private static final RascalWorkspaceService RASCAL_WORKSPACE_SERVICE = new RascalWorkspaceService();
-    private int errorCode = 1;
     private static int portNumber = 8888;
 
-    @Override
-    public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
-        logger.info("LSP connection started");
-        final InitializeResult initializeResult = new InitializeResult(new ServerCapabilities());
-
-        RASCAL_TEXT_DOCUMENT_SERVICE.initializeServerCapabilities(initializeResult.getCapabilities());
-        return CompletableFuture.supplyAsync(() -> initializeResult);
-    }
-
-    @Override
-    public CompletableFuture<Object> shutdown() {
-        this.errorCode = 0;
-        RASCAL_TEXT_DOCUMENT_SERVICE.shutdown();
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
-    public void exit() {
-        System.exit(errorCode);
-    }
-
-    @Override
-    public RascalTextDocumentService getTextDocumentService() {
-        return RASCAL_TEXT_DOCUMENT_SERVICE;
-    }
-
-    @Override
-    public WorkspaceService getWorkspaceService() {
-        return RASCAL_WORKSPACE_SERVICE;
-    }
-
-    @Override
-    public void connect(LanguageClient client) {
-        getTextDocumentService().connect(client);
-    }
-
-    private static Launcher<LanguageClient> constructLSPClient(Socket client, RascalLanguageServer server)
+    private static Launcher<LanguageClient> constructLSPClient(Socket client, ActualServer server)
         throws IOException {
         return constructLSPClient(client.getInputStream(), client.getOutputStream(), server);
     }
 
     private static Launcher<LanguageClient> constructLSPClient(InputStream in, OutputStream out,
-        RascalLanguageServer server) {
+        ActualServer server) {
         Launcher<LanguageClient> clientLauncher = LSPLauncher.createServerLauncher(server, in, out);
         server.connect(clientLauncher.getRemoteProxy());
         return clientLauncher;
@@ -89,17 +45,10 @@ public class RascalLanguageServer implements LanguageServer, LanguageClientAware
     @SuppressWarnings({"java:S2189", "java:S106"})
     public static void main(String[] args) {
         boolean deployMode = false;
-        Level logLevel = Level.INFO;
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "--deploy":
                     deployMode = true;
-                    break;
-                case "--debug":
-                    logLevel = Level.DEBUG;
-                    break;
-                case "--trace":
-                    logLevel = Level.TRACE;
                     break;
             }
         }
@@ -112,19 +61,16 @@ public class RascalLanguageServer implements LanguageServer, LanguageClientAware
             System.setIn(new ByteArrayInputStream(new byte[0]));
             System.setOut(System.err);
         }
-        else {
-            setLoggingLevel(logLevel);
-        }
         logger.info("Starting Rascal Language Server");
 
         if (deployMode) {
-            startLSP(constructLSPClient(in, out, new RascalLanguageServer()));
+            startLSP(constructLSPClient(in, out, new ActualServer()));
         }
         else {
             try (ServerSocket serverSocket = new ServerSocket(portNumber, 0, InetAddress.getByName("127.0.0.1"))) {
                 logger.info("Rascal LSP server listens on port number: {}", portNumber);
                 while (true) {
-                    startLSP(constructLSPClient(serverSocket.accept(), new RascalLanguageServer()));
+                    startLSP(constructLSPClient(serverSocket.accept(), new ActualServer()));
                 }
             } catch (IOException e) {
                 logger.fatal("Failure to start TCP server", e);
@@ -146,11 +92,48 @@ public class RascalLanguageServer implements LanguageServer, LanguageClientAware
 
 
 
-    private static void setLoggingLevel(Level level) {
-        LoggerContext context = (LoggerContext) LogManager.getContext(false);
-        Configuration config = context.getConfiguration();
-        LoggerConfig rootConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
-        rootConfig.setLevel(level);
-        context.updateLoggers();
+    private static final class ActualServer implements LanguageServer, LanguageClientAware {
+        private static final Logger logger = LogManager.getLogger(ActualServer.class);
+
+        private int errorCode = 1;
+        private static final RascalTextDocumentService RASCAL_TEXT_DOCUMENT_SERVICE = new RascalTextDocumentService();
+        private static final RascalWorkspaceService RASCAL_WORKSPACE_SERVICE = new RascalWorkspaceService();
+
+        @Override
+        public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+            logger.info("LSP connection started");
+            final InitializeResult initializeResult = new InitializeResult(new ServerCapabilities());
+
+            RASCAL_TEXT_DOCUMENT_SERVICE.initializeServerCapabilities(initializeResult.getCapabilities());
+            return CompletableFuture.supplyAsync(() -> initializeResult);
+        }
+
+        @Override
+        public CompletableFuture<Object> shutdown() {
+            this.errorCode = 0;
+            RASCAL_TEXT_DOCUMENT_SERVICE.shutdown();
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public void exit() {
+            System.exit(errorCode);
+        }
+
+        @Override
+        public RascalTextDocumentService getTextDocumentService() {
+            return RASCAL_TEXT_DOCUMENT_SERVICE;
+        }
+
+        @Override
+        public WorkspaceService getWorkspaceService() {
+            return RASCAL_WORKSPACE_SERVICE;
+        }
+
+        @Override
+        public void connect(LanguageClient client) {
+            getTextDocumentService().connect(client);
+        }
+
     }
 }
