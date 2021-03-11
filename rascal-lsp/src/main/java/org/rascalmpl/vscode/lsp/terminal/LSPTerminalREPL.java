a1c04e746208ceea1aaa6bcd8076fd4dc705050b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-03-11 17:23:46 +0100

    added empty main class

---
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/terminal/LSPTerminalREPL.java
@@ -0,0 +1,12 @@
+package org.rascalmpl.vscode.lsp.terminal;
+
+/** 
+ * This class runs a Rascal terminal REPL that
+ * connects to a running LSP server instance to 
+ * provide IDE feature to the user of a terminal instance.
+ */
+public class LSPTerminalREPL {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
