73855d833c6da88db63c3551efdabe0c78690afd
Davy Landman <davy.landman@swat.engineering>
2022-11-15 13:17:32 +0100

    Removed some comments that come from the scafolding tool

--- rascal-vscode-extension/webpack.config.js
+++ rascal-vscode-extension/webpack.config.js
@@ -23,7 +23,6 @@ const extensionConfig = {
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: ['.ts', '.js']
   },
   module: {
