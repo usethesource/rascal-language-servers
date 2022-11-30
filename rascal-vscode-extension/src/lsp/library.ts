b7875b206862854329dcf1b7882494aeda2b0581
Davy Landman <davy.landman@swat.engineering>
2022-11-30 17:17:27 +0100

    Making sure to export all the needed classes for npm

--- rascal-vscode-extension/src/lsp/library.ts
+++ rascal-vscode-extension/src/lsp/library.ts
@@ -24,30 +24,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-const fs = require("fs");
-
-let packageContents = JSON.parse(fs.readFileSync("package.json"));
-
-// first we remove some stuff
-delete packageContents.contributes;
-delete packageContents.categories;
-delete packageContents.activationEvents;
-
-// then we change some stuff
-packageContents["name"] = "@usethesource/rascal-vscode-dsl-runtime";
-packageContents["displayName"] = "Rascal Metaprogramming Language - VSCode DSL Runtime library";
-packageContents["description"] = "This package is intended for if you want to release your Rascal DSL as a separate VS Code extension, you are responsible for the initial setup, but this package will provide you a LSP server ready for action.";
-
-// make new scripts block with just the things we need to publish to npm
-packageContents.scripts = {
-  "precompile" : packageContents.scripts["lsp4j:package"],
-  "compile" : packageContents.scripts["compile-lib"],
-  "prepare": "npm run compile",
-};
-
-// we add wich files we actually want to add
-packageContents["main"] = "lib/lsp/library.js";
-packageContents["types"] = "lib/lsp/library.d.ts";
-packageContents["files"] = ["lib/**/*", "assets/**/*"];
-
-fs.writeFileSync("package.json", JSON.stringify(packageContents, null, 2));
+// make sure that everything you might neeed to run `ParameterizedLanguageServer` is exported in this file
+export { ParameterizedLanguageServer, LanguageParameter } from "./ParameterizedLanguageServer";
+export { VSCodeUriResolverServer } from "../fs/VSCodeURIResolver";
