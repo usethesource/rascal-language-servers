9ed7e72636866e285d217c3914c0dc3f81877935
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2023-05-31 14:17:03 +0200

    added underscore back in, via @davylandman

--- rascal-vscode-extension/src/ux/RascalMFValidator.ts
+++ rascal-vscode-extension/src/ux/RascalMFValidator.ts
@@ -152,7 +152,7 @@ function checkIncorrectProjectName(mfBody: vscode.TextDocument, diagnostics: vsc
             }
             if (INVALID_PROJECT_NAME.test(prName)) {
                 const diag = new vscode.Diagnostic(targetRange,
-                    "Can not handle project names that are not all lowercase, digits, or dashes, i.e. in [a-z0-9\\-] + (" + INVALID_PROJECT_NAME + ")", vscode.DiagnosticSeverity.Error);
+                    "Can not handle project name (" + INVALID_PROJECT_NAME + ") that is not all lowercase, digits, or dashes, i.e. in [a-z0-9\\-\\_]", vscode.DiagnosticSeverity.Error);
                 diag.code = FixKind.removeInvalidCharsProjectName;
                 diagnostics.push(diag);
             }
