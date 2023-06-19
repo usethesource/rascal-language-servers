ab0dd947e68824a7136425803e8c585bba42272a
yami2200 <TheYami22@gmail.com>
2023-06-19 14:41:22 +0200

    refactor: start new thread creation in Launcher class.

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/dap/RascalDebugAdapterLauncher.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/dap/RascalDebugAdapterLauncher.java
@@ -7,6 +7,7 @@ import org.eclipse.lsp4j.debug.services.IDebugProtocolClient;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.rascalmpl.debug.AbstractInterpreterEventTrigger;
 import org.rascalmpl.debug.DebugHandler;
+import org.rascalmpl.interpreter.Evaluator;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -17,19 +18,34 @@ import java.util.concurrent.ExecutionException;
 public class RascalDebugAdapterLauncher {
 
 
-    public static void startDebugServer(AbstractInterpreterEventTrigger eventTrigger, DebugHandler debugHandler) {
+    public static void startDebugServer(AbstractInterpreterEventTrigger eventTrigger, DebugHandler debugHandler, Evaluator evaluator) {
         try {
             ServerSocket serverSocket = new ServerSocket(8889);
-            RascalDebugAdapterServer server = new RascalDebugAdapterServer(eventTrigger, debugHandler);
+            RascalDebugAdapterServer server = new RascalDebugAdapterServer(eventTrigger, debugHandler, evaluator);
 
-            Socket socket = serverSocket.accept();
-                    
-            Launcher<IDebugProtocolClient> launcher = DSPLauncher.createServerLauncher(server, socket.getInputStream(), socket.getOutputStream());
-            server.connect(launcher.getRemoteProxy());
-            launcher.startListening();
+            Thread t = new Thread() {
+                public void run() {
+                    Socket socket = null;
+                    try {
+                        socket = serverSocket.accept();
+
+                        Launcher<IDebugProtocolClient> launcher = DSPLauncher.createServerLauncher(server, socket.getInputStream(), socket.getOutputStream());
+                        server.connect(launcher.getRemoteProxy());
+                        launcher.startListening();
+                    } catch (IOException e) {
+                        final Logger logger = LogManager.getLogger(RascalDebugAdapterLauncher.class);
+                        logger.fatal(e.getMessage(), e);
+                        throw new RuntimeException(e);
+                    }
+                }
+            };
+            t.setDaemon(true);
+            t.start();
+
         } catch (Throwable e) {
             final Logger logger = LogManager.getLogger(RascalDebugAdapterLauncher.class);
             logger.fatal(e.getMessage(), e);
+            throw new RuntimeException(e);
         }
     }
 }
