01f862bf86e7cdbc7f783e0001f6a1ffc7bb45f7
Davy Landman <davy.landman@swat.engineering>
2023-09-25 21:16:09 +0200

    Got initial automated testing to work

--- rascal-vscode-extension/src/test/vscode-suite/extension.test.ts
+++ rascal-vscode-extension/src/test/vscode-suite/extension.test.ts
@@ -24,29 +24,112 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-import * as assert from 'assert';
-import * as vscode from 'vscode';
 
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+import { assert, expect,  } from 'chai';
+// import the webdriver and the high level browser wrapper
+import { BottomBarPanel, By, TerminalView, VSBrowser, WebDriver, Workbench } from 'vscode-extension-tester';
+import { integer } from 'vscode-languageclient';
 
-    suiteSetup(async function() {
-        const extension = await vscode.extensions.getExtension('usethesource.rascalmpl');
-        if (!extension) {
-            throw new Error("Could not initialize extension");
+
+// Create a Mocha suite
+describe('Rascal VS Code extension', function () {
+    let browser: VSBrowser;
+    let driver: WebDriver;
+    let bench: Workbench;
+
+    this.timeout(20000);
+
+    // initialize the browser and webdriver
+    before(async () => {
+        browser = VSBrowser.instance;
+        driver = browser.driver;
+        bench = new Workbench();
+        await browser.waitForWorkbench();
+    });
+
+    describe('REPL', () => {
+        let terminal: TerminalView;
+        let panel: BottomBarPanel;
+        const REPL_CREATE_TIMEOUT = 10_000;
+        const REPL_READY_TIMEOUT = 10_000;
+
+        before(async () => {
+            /* make bottom panel in focus */
+            panel = new BottomBarPanel();
+            await panel.toggle(true);
+            terminal = await panel.openTerminalView();
+            await sleep(2000);
+        });
+
+        after(async () => {
+            await terminal.killTerminal();
+            await panel.toggle(false);
+        });
+
+        afterEach(async() => {
+            await terminal.killTerminal();
+        });
+
+        async function isRascalReplActive() {
+            for (let tries = 0; tries < 20; tries++) {
+                if ((/rascal/i).test(await terminal.getCurrentChannel())) {
+                    return true;
+                }
+                await sleep(REPL_CREATE_TIMEOUT / 20);
+            }
+            return false;
         }
-		extension.activate();
-	});
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-non-null-assertion
-    const rascalExtension:vscode.Extension<any> = vscode.extensions.getExtension('usethesource.rascalmpl')!;
+        async function waitForReplReady() {
+            for (let tries = 0; tries < 5; tries++) {
+                await sleep(REPL_READY_TIMEOUT / 5);
+                if (/rascal>\s*$/.test(await terminal.getText())) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-    test("Rascal extension is started", () => {
-        assert.strictEqual(rascalExtension.isActive, true);
+        async function createRascalTerminal() {
+            await bench.executeCommand("rascalmpl.createTerminal");
+            assert(await isRascalReplActive(), "Terminal should be opened");
+            assert(await waitForReplReady(), "Repl prompt should print");
+        }
+
+        async function execute(command: string, waitForReady = true) {
+            const inputs = await driver.findElements(By.className('xterm-helper-textarea'));
+            //console.log(inputs);
+            for (const i of inputs) {
+                try {
+                    await i.clear();
+                    try {
+                        await i.sendKeys(command +'\n');
+                    } catch (_ignore) { /* ignore, might other terminal */ }
+                } catch (_ignore) { /* ignore, might be toher terminal */ }
+            }
+            if (waitForReady) {
+                assert(await waitForReplReady());
+            }
+
+        }
+
+        it ("should open without a project", async () => {
+            await createRascalTerminal();
+        }).timeout(REPL_CREATE_TIMEOUT + REPL_READY_TIMEOUT);
+
+        it ("run basic rascal commands", async () => {
+            await createRascalTerminal();
+            await execute("40 + 2");
+            assert.match(await terminal.getText(), /int.*42/, "Result of expression should be 42");
+            await execute("import IO;");
+            await execute('println("Printing works: <1 + 3>");');
+            assert.match(await terminal.getText(), /Printing works: 4/, "println works as expected");
+        }).timeout(REPL_CREATE_TIMEOUT + REPL_READY_TIMEOUT);
+
     });
-
-    test('Never commit debug mode', () => {
-        assert.strictEqual(rascalExtension.exports.getRascalExtensionDeploymode(), true);
-    });
-
 });
+
+
+async function sleep(ms: number) {
+    return new Promise(r => setTimeout(r, ms));
+}
