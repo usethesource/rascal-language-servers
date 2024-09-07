16e2b94a9cbb4b1b877929c5412a2fddf7abe499
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2024-09-07 15:25:25 +0200

    documented commands for running ui tests on the commandline. Note that on Mac at least you can not multitask while the tests are running

---
+++ runUItests.sh
@@ -0,0 +1,26 @@
+# !/bin/sh
+
+# This documents (and performs) necessary commands for local UI testing.
+# Experts might run these manually on demand. For example, repeatedly
+# running the final npx extest command for debugging purposes is normal.
+
+set -e;
+set -x;
+
+cd rascal-vscode-extension
+
+UITESTS=/tmp/vscode-uitests
+
+# cleanup to avoid contamination with previous runs
+
+rm -rf out || true
+rm -rf node_modules || true
+rm -rf $UITESTS || true
+
+# compiling the TS code as well as the test TS code at least once is required before execution
+npm ci
+npm run compile-tests
+
+# test what was compiled
+
+exec npx extest setup-and-run out/test/vscode-suite/*.test.js --storage $UITESTS
