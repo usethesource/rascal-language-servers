726ace43f8577a8d4d53e43a7be12b630bdd1308
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2024-11-08 08:56:20 +0100

    Move value factory to class field

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/SemanticTokenizerTester.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/SemanticTokenizerTester.java
@@ -32,23 +32,24 @@ import org.rascalmpl.values.parsetrees.ITree;
 import io.usethesource.vallang.IBool;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IValue;
+import io.usethesource.vallang.IValueFactory;
 
 public class SemanticTokenizerTester {
+    private static final IValueFactory VF = IRascalValueFactory.getInstance();
 
     public IValue toTokens(IConstructor tree, IBool useSpecialCaseHighlighting, IBool applyRascalCategoryPatch) {
         var tokenizer = new SemanticTokenizer(applyRascalCategoryPatch.getValue());
         var encoded = tokenizer.semanticTokensFull((ITree) tree, useSpecialCaseHighlighting.getValue()).getData();
 
-        var values = IRascalValueFactory.getInstance();
-        var tokens = values.listWriter();
+        var tokens = VF.listWriter();
         var tokenTypes = tokenizer.capabilities().getTokenTypes();
 
         for (int i = 0; i < encoded.size(); i += 5) {
-            var deltaLine     = values.integer(encoded.get(i));
-            var deltaStart    = values.integer(encoded.get(i + 1));
-            var length        = values.integer(encoded.get(i + 2));
-            var tokenType     = values.string(tokenTypes.get(encoded.get(i + 3)));
-            var tokenModifier = values.string(""); // Token modifiers aren't supported yet
+            var deltaLine     = VF.integer(encoded.get(i));
+            var deltaStart    = VF.integer(encoded.get(i + 1));
+            var length        = VF.integer(encoded.get(i + 2));
+            var tokenType     = VF.string(tokenTypes.get(encoded.get(i + 3)));
+            var tokenModifier = VF.string(""); // Token modifiers aren't supported yet
             tokens.appendTuple(deltaLine, deltaStart, length, tokenType, tokenModifier);
         }
 
