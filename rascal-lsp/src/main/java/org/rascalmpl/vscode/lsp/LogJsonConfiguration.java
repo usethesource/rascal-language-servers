bf224dc1cd2cea3e7b20942c84f7052f401ab2c5
Toine Hartman <toine.hartman@swat.engineering>
2025-08-20 11:29:55 +0200

    Parsing/logging of JSON messages.

---
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/LogJsonConfiguration.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2018-2025, NWO-I CWI and Swat.engineering
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.rascalmpl.vscode.lsp;
+
+import java.net.URI;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+
+public class LogJsonConfiguration extends ConfigurationFactory {
+
+    @Override
+    protected String[] getSupportedTypes() {
+        return new String[] {"*"};
+    }
+
+
+
+    @Override
+    public Configuration getConfiguration(LoggerContext loggerContext, String name, URI configLocation) {
+        return buildConfiguration();
+    }
+
+    @Override
+    public Configuration getConfiguration(LoggerContext loggerContext, String name, URI configLocation,
+            ClassLoader loader) {
+        return buildConfiguration();
+    }
+
+    @Override
+    public Configuration getConfiguration(LoggerContext loggerContext, ConfigurationSource source) {
+        return buildConfiguration();
+    }
+
+    private Configuration buildConfiguration() {
+        Level targetLevel = Level.getLevel(System.getProperty("log4j2.level", "INFO"));
+        if (targetLevel == null) {
+            targetLevel = Level.INFO;
+        }
+
+        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+        builder.setConfigurationName("JsonLogger");
+        builder.setStatusLevel(targetLevel);
+
+        builder.add(builder
+            .newAppender("Console", ConsoleAppender.PLUGIN_NAME)
+            .addAttribute("target", ConsoleAppender.Target.SYSTEM_ERR)
+            .addAttribute("immediateFlush", true)
+            .add(builder.newLayout("JsonTemplateLayout"))
+        );
+
+        builder.add(builder
+            .newRootLogger(targetLevel)
+            .add(builder.newAppenderRef("Console"))
+        );
+
+        return builder.build();
+    }
+
+}
--- rascal-vscode-extension/src/lsp/RascalLSPConnection.ts
+++ rascal-vscode-extension/src/lsp/RascalLSPConnection.ts
@@ -24,17 +24,192 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-import * as vscode from 'vscode';
 import * as net from 'net';
 import * as os from 'os';
 import * as path from 'path';
+import * as vscode from 'vscode';
 
 import { integer, LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo } from 'vscode-languageclient/node';
 import { getJavaExecutable } from '../auto-jvm/JavaLookup';
 import { RascalFileSystemProvider } from '../fs/RascalFileSystemProviders';
 import { VSCodeUriResolverServer } from '../fs/VSCodeURIResolver';
 
+enum LogLevel {
+    fatal = "FATAL",
+    error = "ERROR",
+    warn = "WARN",
+    info = "INFO",
+    debug = "DEBUG",
+    trace = "TRACE",
+}
 
+class JsonLogMessage {
+    readonly timestamp: Date;
+    readonly loglevel: LogLevel;
+    readonly message: string;
+    readonly threadName: string;
+    readonly loggerName: string;
+
+    constructor(timestamp: Date, loglevel: LogLevel, message: string, threadName: string, loggerName: string) {
+        this.timestamp = timestamp;
+        this.loglevel = loglevel;
+        this.message = message;
+        this.threadName = threadName;
+        this.loggerName = loggerName;
+    }
+
+    static parse(str: string): JsonLogMessage {
+        const obj = JSON.parse(str);
+        const timestamp: Date = new Date(obj["@timestamp"]);
+        const loglevel: LogLevel = obj["log.level"];
+        const message: string = obj["message"];
+        const threadName: string = obj["process.thread.name"];
+        const loggerName: string = obj["log.logger"];
+
+        if (!(timestamp && loglevel && message && threadName && loggerName)) {
+            throw new Error(`Could not parse JSON:\n${str}`);
+        }
+
+        return new JsonLogMessage(timestamp, loglevel, message, threadName, loggerName);
+    }
+}
+
+class JsonParserOutputChannel implements vscode.OutputChannel {
+    readonly title: string;
+
+    private readonly nested: vscode.LogOutputChannel;
+    private readonly threadBuffers: Map<string, string>;
+
+    // https://logging.apache.org/log4j/2.x/manual/json-template-layout.html#plugin-attr-truncatedStringSuffix
+    private static readonly truncationSymbol = "…";
+    private static readonly truncationSymbolLength = this.truncationSymbol.length;
+
+    constructor(name: string) {
+        this.nested = vscode.window.createOutputChannel(name, {log: true});
+        this.title = name;
+        this.threadBuffers = new Map();
+    }
+
+    private printBufferedMessage(json: JsonLogMessage): void {
+        // no timestamp or log level, since LogOutputChannel functions add those
+        const message = this.getAndResetBuffer(json.threadName);
+        // this.nested.appendLine(`Buffered message: ${message}`);
+        const log = `[${json.threadName}] ${json.loggerName} ${message}`;
+        switch (json.loglevel) {
+            case LogLevel.fatal: // intentional fall-trough
+            case LogLevel.error: {
+                this.nested.error(log);
+                break;
+            }
+            case LogLevel.warn: {
+                this.nested.warn(log);
+                break;
+            }
+            case LogLevel.info: {
+                this.nested.info(log);
+                break;
+            }
+            case LogLevel.debug: {
+                this.nested.debug(log);
+                break;
+            }
+            case LogLevel.trace: {
+                this.nested.trace(log);
+                break;
+            }
+            default: {
+                this.nested.appendLine(`[NOT WRAPPED] ${json.loglevel} ${log}`);
+            }
+        }
+    }
+
+    private getAndResetBuffer(threadName: string): string | undefined {
+        const buffer = this.threadBuffers.get(threadName);
+        this.threadBuffers.set(threadName, "");
+        return buffer;
+    }
+
+    private appendBuffer(threadName: string, suffix: string): string {
+        const curr = this.threadBuffers.get(threadName);
+        const appended = curr + suffix;
+        this.threadBuffers.set(threadName, appended);
+        // this.nested.appendLine(`Buffer of [${threadName}] now has size ${appended.length}`);
+        return appended;
+    }
+
+    private printJsonPayloads(payload: string): void {
+        // this.nested.appendLine(`Payload: ${payload}`);
+        const jsons = payload.trim().split("\n");
+        // let i = 1;
+        for (const json of jsons) {
+            // this.nested.appendLine(`Printing JSON ${i++}/${jsons.length}: ${json}`);
+            this.printPayload(json);
+        }
+    }
+
+    private printPayload(payload: string): void {
+        try {
+            // this.nested.appendLine(`Trying to print as JSON: ${payload}`);
+            this.printJsonPayLoad(payload);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                // this was not JSON at all... fall back to default behaviour
+                this.nested.appendLine(payload);
+            } else {
+                this.nested.appendLine(`Error while logging ${payload}: ${e}`);
+                throw e;
+            }
+        }
+    }
+
+    private cleanMessage(message: string): [string, boolean] {
+        if (message.endsWith(JsonParserOutputChannel.truncationSymbol)) {
+            return [message.slice(0, -JsonParserOutputChannel.truncationSymbolLength), true];
+        }
+        return [message, false];
+    }
+
+    private printJsonPayLoad(payload: string): void {
+        const json: JsonLogMessage = JsonLogMessage.parse(payload);
+        if (!this.threadBuffers.has(json.threadName)) {
+            this.threadBuffers.set(json.threadName, "");
+        }
+
+        const [message, isTruncated] = this.cleanMessage(json.message);
+        if (isTruncated) {
+            this.appendBuffer(json.threadName, message);
+        } else {
+            this.appendBuffer(json.threadName, message);
+            this.printBufferedMessage(json);
+        }
+    }
+
+    append(payload: string): void {
+        // console.log(`append (${payload.length}): ${payload}`);
+        this.printJsonPayloads(payload);
+    }
+
+    appendLine(payload: string): void {
+        // console.log(`appendLine (${payload.length}): ${payload}`);
+        this.printJsonPayloads(payload);
+    }
+
+    show(preserveFocus?: unknown): void {
+        this.nested.show(preserveFocus as boolean);
+    }
+    replace(value: string): void {
+        this.nested.replace(value);
+    }
+    clear(): void {
+        this.nested.clear();
+    }
+    hide(): void {
+        this.nested.hide();
+    }
+    dispose(): void {
+        this.nested.dispose();
+    }
+}
 
 export async function activateLanguageClient(
     { language, title, jarPath, vfsServer, isParametricServer = false, deployMode = true, devPort = -1, dedicated = false, lspArg = "" } :
@@ -47,6 +222,7 @@ export async function activateLanguageClient(
 
     const clientOptions = <LanguageClientOptions>{
         documentSelector: [{ scheme: '*', language: language }],
+        outputChannel: new JsonParserOutputChannel(title)
     };
 
     const client = new LanguageClient(language, title, serverOptions, clientOptions, !deployMode);
@@ -125,8 +301,8 @@ interface BrowseParameter {
 async function buildRascalServerOptions(jarPath: string, isParametricServer: boolean, dedicated: boolean, lspArg : string | undefined): Promise<ServerOptions> {
     const classpath = buildCompilerJVMPath(jarPath);
     const commandArgs = [
-        '-Dlog4j2.configurationFactory=org.rascalmpl.vscode.lsp.LogRedirectConfiguration'
-        , '-Dlog4j2.level=DEBUG'
+        '-Dlog4j2.configurationFactory=org.rascalmpl.vscode.lsp.LogJsonConfiguration'
+        , '-Dlog4j2.level=DEBUG' // TODO Remove this minimum
         , '-Drascal.fallbackResolver=org.rascalmpl.vscode.lsp.uri.FallbackResolver'
         , '-Drascal.lsp.deploy=true'
         , '-Drascal.compilerClasspath=' + classpath
