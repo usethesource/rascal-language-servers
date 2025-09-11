usethesource/rascal-language-servers

commit 89b2768da129b2d868b2347aee7cbfcd30012357

Make sure messages are only reported for files that are in the workspace (#735)
* Make sure messages are only reported for files that are in the workspace

* Cleaned up the mapping code

* Processed up comments from PR

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/RascalLanguageServices.java
@@ -28,13 +28,11 @@ package org.rascalmpl.vscode.lsp.rascal;
 
 import static org.rascalmpl.vscode.lsp.util.EvaluatorUtil.makeFutureEvaluator;
 import static org.rascalmpl.vscode.lsp.util.EvaluatorUtil.runEvaluator;
-
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -42,8 +40,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -75,7 +71,6 @@ import org.rascalmpl.vscode.lsp.util.EvaluatorUtil.LSPContext;
 import org.rascalmpl.vscode.lsp.util.RascalServices;
 import org.rascalmpl.vscode.lsp.util.concurrent.InterruptibleFuture;
 import org.rascalmpl.vscode.lsp.util.locations.Locations;
-
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IList;
 import io.usethesource.vallang.ISet;
@@ -147,26 +142,6 @@ public class RascalLanguageServices {
         }
     }
 
-    public InterruptibleFuture<Map<ISourceLocation, ISet>> compileFolder(ISourceLocation folder, PathConfig pcfg,
-        Executor exec) {
-        return runEvaluator("Rascal checkAll", compilerEvaluator,
-            e -> {
-                var config = e.call("getRascalCoreCompilerConfig", pcfg.asConstructor());
-                return translateCheckResults((ISet) e.call("checkAll", folder, config));
-            },
-            Collections.emptyMap(), exec, false, client);
-    }
-
-    private static Map<ISourceLocation, ISet> translateCheckResults(ISet messages) {
-        logger.trace("Translating messages: {}", messages);
-        return messages.stream()
-            .filter(IConstructor.class::isInstance)
-            .map(IConstructor.class::cast)
-            .collect(Collectors.toMap(
-                c -> (ISourceLocation) c.get("src"),
-                c -> (ISet) c.get("messages")));
-    }
-
     IFunction makePathConfigGetter(Evaluator e) {
         return e.getFunctionValueFactory().function(getPathConfigType, (t, u) ->
             rascalTextDocumentService.getFileFacts().getPathConfig((ISourceLocation) t[0]).asConstructor());
@@ -196,21 +171,16 @@ public class RascalLanguageServices {
         });
     }
 
-    public InterruptibleFuture<Map<ISourceLocation, ISet>> compileFile(ISourceLocation file, PathConfig pcfg,
+    public InterruptibleFuture<ISet> compileFile(ISourceLocation file, PathConfig pcfg,
         Executor exec) {
         logger.debug("Running Rascal check for: {} with {}", file, pcfg);
         var workspaceFolders = workspaceService.workspaceFolders().stream().map(f -> Locations.toLoc(f.getUri())).collect(VF.setWriter());
 
         return runEvaluator("Rascal check", compilerEvaluator,
-            e -> translateCheckResults((ISet) e.call("checkFile", file, workspaceFolders, makeParseTreeGetter(e), makePathConfigGetter(e))),
-            buildEmptyResult(VF.list(file)), exec, false, client);
+            e -> (ISet) e.call("checkFile", file, workspaceFolders, makeParseTreeGetter(e), makePathConfigGetter(e)),
+            VF.set(), exec, false, client);
     }
 
-    private Map<ISourceLocation, ISet> buildEmptyResult(IList files) {
-        return files.stream()
-            .map(ISourceLocation.class::cast)
-            .collect(Collectors.toMap(k -> k, k -> VF.set()));
-    }
 
     private ISourceLocation getFileLoc(ITree moduleTree) {
         try {

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/model/FileFacts.java
@@ -152,11 +152,7 @@ public class FileFacts {
             typeCheckerMessages.clear();
             this.typeCheckResults.replace(
                 rascal.compileFile(file, confs.lookupConfig(file), exec)
-                    .thenApply(m -> {
-                        Map<ISourceLocation, List<Diagnostic>> result = new HashMap<>(m.size());
-                        m.forEach((l, msgs) -> result.put(l, Diagnostics.translateDiagnostics(l, msgs, cm)));
-                        return result;
-                    })
+                    .thenApply(messages -> Diagnostics.translateMessages(messages, cm))
             ).thenAccept(m -> m.forEach((f, msgs) -> getFile(f).reportTypeCheckerErrors(msgs)));
         }
 

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/Diagnostics.java
@@ -28,12 +28,12 @@ package org.rascalmpl.vscode.lsp.util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.lsp4j.Diagnostic;
@@ -224,34 +224,13 @@ public class Diagnostics {
         return Locations.toRange(loc, cm);
     }
 
-    public static List<Diagnostic> translateDiagnostics(ISourceLocation file, ICollection<?> messages, ColumnMaps cm) {
+    public static Map<ISourceLocation, List<Diagnostic>> translateMessages(ICollection<?> messages, ColumnMaps cm) {
         return messages.stream()
             .filter(IConstructor.class::isInstance)
             .map(IConstructor.class::cast)
-            .filter(d -> Diagnostics.hasValidLocation(d, file))
-            .map(d -> translateDiagnostic(d, cm))
-            .collect(Collectors.toList());
-    }
-
-    /**
-     * Indexes all messages per file and translates them to LSP Diagnostic representation
-     * @param messages    as in the `Message` standard library module
-     * @param docService  needed to convert column positions
-     * @return an ordered map of Diagnostics
-     */
-    public static Map<ISourceLocation, List<Diagnostic>> translateMessages(IList messages, ColumnMaps cm) {
-        Map<ISourceLocation, List<Diagnostic>> results = new HashMap<>();
-
-        for (IValue elem : messages) {
-            IConstructor message = (IConstructor) elem;
-            ISourceLocation file = getMessageLocation(message).top();
-            Diagnostic d = translateDiagnostic(message, cm);
-
-            List<Diagnostic> lst = results.computeIfAbsent(file, l -> new LinkedList<>());
-            lst.add(d);
-        }
-
-        return results;
+            .filter(Diagnostics::hasValidLocation)
+            .map(d -> Pair.of(getMessageLocation(d), translateDiagnostic(d, cm)))
+            .collect(Collectors.groupingBy(Pair::getLeft, Collectors.mapping(Pair::getRight, Collectors.toList())));
     }
 
     private static ISourceLocation getMessageLocation(IConstructor message) {
@@ -262,18 +241,23 @@ public class Diagnostics {
         return ((IString) msg.get("msg")).getValue();
     }
 
-    private static boolean hasValidLocation(IConstructor d, ISourceLocation file) {
+    private static boolean hasValidLocation(IConstructor d) {
         ISourceLocation loc = getMessageLocation(d);
 
         if (loc == null || loc.getScheme().equals("unknown")) {
             logger.trace("Dropping diagnostic due to incorrect location on message: {}", d);
             return false;
         }
-
-        if (!loc.top().equals(file.top())) {
-            logger.trace("Dropping diagnostic, reported for the wrong file: {}, message: {}", file, d);
-            return false;
+        if (loc.getPath().endsWith(".rsc")) {
+            return true;
         }
-        return true;
+        if (loc.getPath().endsWith("/RASCAL.MF")) {
+            return true;
+        }
+        if (loc.getPath().endsWith("/pom.xml")) {
+            return true;
+        }
+        logger.error("Filtering diagnostic as it's an unsupported file to report diagnostics on: {}", d);
+        return false;
     }
 }

rascal-lsp/src/main/rascal/lsp/lang/rascal/lsp/IDECheckerWrapper.rsc
@@ -50,8 +50,8 @@ import lang::rascalcore::check::ModuleLocations;
     source locations can occur in the `libs` entry of the PathConfig of a project. Note that for `lib`
     locations, the type checker uses `tpl` files that are packaged with libraries.
 }
-set[ModuleMessages] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file) getParseTree, PathConfig(loc file) getPathConfig)
-    = job("Rascal check", set[ModuleMessages](void(str, int) step) {
+set[Message] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file) getParseTree, PathConfig(loc file) getPathConfig)
+    = job("Rascal check", set[Message](void(str, int) step) {
     checkForImports = [getParseTree(l)];
     checkedForImports = {};
     initialProject = inferProjectRoot(l);
@@ -104,7 +104,7 @@ set[ModuleMessages] checkFile(loc l, set[loc] workspaceFolders, start[Module](lo
 
     step("Checking module <l>", 1);
     msgs += check([l], rascalCompilerConfig(getPathConfig(initialProject)));
-    return {*msgs};
+    return { m | program(_, messages) <- msgs, m <- messages, inWorkspace(workspaceFolders, m.at)};
 }, totalWork=3);
 
 private bool inWorkspace(set[loc] workspaceFolders, loc lib) {

commit 0a385fefa0dff1683f0cb0297b8baed05854a9a4

Fixed diagnostic merging (#739)
* Proper implementation of error filtering and fixing

Fixes error introduced by #735 / 89b2768da129b2d868b2347aee7cbfcd30012357

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/RascalLanguageServices.java
@@ -33,6 +33,7 @@ import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -40,6 +41,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -73,6 +75,7 @@ import org.rascalmpl.vscode.lsp.util.concurrent.InterruptibleFuture;
 import org.rascalmpl.vscode.lsp.util.locations.Locations;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IList;
+import io.usethesource.vallang.IMap;
 import io.usethesource.vallang.ISet;
 import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
@@ -142,6 +145,16 @@ public class RascalLanguageServices {
         }
     }
 
+    private static Map<ISourceLocation, ISet> translateCheckResults(IMap messages) {
+        logger.trace("Translating messages: {}", messages);
+        return messages.stream()
+            .map(ITuple.class::cast)
+            .collect(Collectors.toMap(
+                c -> (ISourceLocation) c.get(0),
+                c -> (ISet) c.get(1)
+            ));
+    }
+
     IFunction makePathConfigGetter(Evaluator e) {
         return e.getFunctionValueFactory().function(getPathConfigType, (t, u) ->
             rascalTextDocumentService.getFileFacts().getPathConfig((ISourceLocation) t[0]).asConstructor());
@@ -171,16 +184,15 @@ public class RascalLanguageServices {
         });
     }
 
-    public InterruptibleFuture<ISet> compileFile(ISourceLocation file, PathConfig pcfg,
+    public InterruptibleFuture<Map<ISourceLocation, ISet>> compileFile(ISourceLocation file, PathConfig pcfg,
         Executor exec) {
         logger.debug("Running Rascal check for: {} with {}", file, pcfg);
         var workspaceFolders = workspaceService.workspaceFolders().stream().map(f -> Locations.toLoc(f.getUri())).collect(VF.setWriter());
 
         return runEvaluator("Rascal check", compilerEvaluator,
-            e -> (ISet) e.call("checkFile", file, workspaceFolders, makeParseTreeGetter(e), makePathConfigGetter(e)),
-            VF.set(), exec, false, client);
+            e -> translateCheckResults((IMap) e.call("checkFile", file, workspaceFolders, makeParseTreeGetter(e), makePathConfigGetter(e))),
+            Map.of(file, VF.set()), exec, false, client);
     }
-
 
     private ISourceLocation getFileLoc(ITree moduleTree) {
         try {

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/model/FileFacts.java
@@ -152,7 +152,7 @@ public class FileFacts {
             typeCheckerMessages.clear();
             this.typeCheckResults.replace(
                 rascal.compileFile(file, confs.lookupConfig(file), exec)
-                    .thenApply(messages -> Diagnostics.translateMessages(messages, cm))
+                    .thenApply(m -> Diagnostics.translateMessages(m, cm))
             ).thenAccept(m -> m.forEach((f, msgs) -> getFile(f).reportTypeCheckerErrors(msgs)));
         }
 

rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/Diagnostics.java
@@ -50,6 +50,7 @@ import org.rascalmpl.vscode.lsp.util.locations.Locations;
 import io.usethesource.vallang.ICollection;
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.IList;
+import io.usethesource.vallang.ISet;
 import io.usethesource.vallang.ISourceLocation;
 import io.usethesource.vallang.IString;
 import io.usethesource.vallang.IValue;
@@ -224,6 +225,23 @@ public class Diagnostics {
         return Locations.toRange(loc, cm);
     }
 
+    public static Map<ISourceLocation, List<Diagnostic>> translateMessages(Map<ISourceLocation, ISet> messagesPerModule, ColumnMaps cm) {
+        return messagesPerModule.entrySet().stream()
+            .filter(kv -> isValidLocation(kv.getKey(), kv.getValue()))
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                kv -> translateDiagnostics(kv.getValue(), cm)
+            ));
+    }
+
+    private static List<Diagnostic> translateDiagnostics(ICollection<?> messages, ColumnMaps cm) {
+        return messages.stream()
+            .filter(IConstructor.class::isInstance)
+            .map(IConstructor.class::cast)
+            .map(d -> translateDiagnostic(d, cm))
+            .collect(Collectors.toList());
+    }
+
     public static Map<ISourceLocation, List<Diagnostic>> translateMessages(ICollection<?> messages, ColumnMaps cm) {
         return messages.stream()
             .filter(IConstructor.class::isInstance)
@@ -242,10 +260,12 @@ public class Diagnostics {
     }
 
     private static boolean hasValidLocation(IConstructor d) {
-        ISourceLocation loc = getMessageLocation(d);
+        return isValidLocation(getMessageLocation(d), d);
+    }
 
+    private static boolean isValidLocation( ISourceLocation loc, IValue m) {
         if (loc == null || loc.getScheme().equals("unknown")) {
-            logger.trace("Dropping diagnostic due to incorrect location on message: {}", d);
+            logger.trace("Dropping diagnostic due to incorrect location on message: {}", m);
             return false;
         }
         if (loc.getPath().endsWith(".rsc")) {
@@ -257,7 +277,7 @@ public class Diagnostics {
         if (loc.getPath().endsWith("/pom.xml")) {
             return true;
         }
-        logger.error("Filtering diagnostic as it's an unsupported file to report diagnostics on: {}", d);
+        logger.error("Filtering diagnostic as it's an unsupported file to report diagnostics on: {}", m);
         return false;
     }
 }

rascal-lsp/src/main/rascal/lsp/lang/rascal/lsp/IDECheckerWrapper.rsc
@@ -50,8 +50,8 @@ import lang::rascalcore::check::ModuleLocations;
     source locations can occur in the `libs` entry of the PathConfig of a project. Note that for `lib`
     locations, the type checker uses `tpl` files that are packaged with libraries.
 }
-set[Message] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file) getParseTree, PathConfig(loc file) getPathConfig)
-    = job("Rascal check", set[Message](void(str, int) step) {
+map[loc, set[Message]] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file) getParseTree, PathConfig(loc file) getPathConfig)
+    = job("Rascal check", map[loc, set[Message]](void(str, int) step) {
     checkForImports = [getParseTree(l)];
     checkedForImports = {};
     initialProject = inferProjectRoot(l);
@@ -86,7 +86,7 @@ set[Message] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file)
 
     cyclicDependencies = {p | <p, p> <- (dependencies - ident(carrier(dependencies)))+};
     if (cyclicDependencies != {}) {
-        return {program(l, {error("Cyclic dependencies detected between projects {<intercalate(", ", [*cyclicDependencies])>}. This is not supported. Fix your project setup.", l)})};
+        return (l : {error("Cyclic dependencies detected between projects {<intercalate(", ", [*cyclicDependencies])>}. This is not supported. Fix your project setup.", l)});
     }
     modulesPerProject = classify(checkedForImports, loc(loc l) {return inferProjectRoot(l);});
     msgs = [];
@@ -104,7 +104,7 @@ set[Message] checkFile(loc l, set[loc] workspaceFolders, start[Module](loc file)
 
     step("Checking module <l>", 1);
     msgs += check([l], rascalCompilerConfig(getPathConfig(initialProject)));
-    return { m | program(_, messages) <- msgs, m <- messages, inWorkspace(workspaceFolders, m.at)};
+    return filterAndFix(msgs, workspaceFolders);
 }, totalWork=3);
 
 private bool inWorkspace(set[loc] workspaceFolders, loc lib) {
@@ -156,4 +156,13 @@ loc inferProjectRoot(loc member) {
     }
 
     return current;
+}
+
+map[loc, set[Message]] filterAndFix(list[ModuleMessages] messages, set[loc] workspaceFolders) {
+    set[Message] empty = {};
+    map[loc, set[Message]] result = ( f.top : empty | program(f,_) <- messages);
+    for (program(_, ms) <- messages, m <- ms, inWorkspace(workspaceFolders, m.at.top)) {
+        result[m.at.top]?empty += {m};
+    }
+    return result;
 }