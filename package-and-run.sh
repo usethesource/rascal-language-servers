dc0d0520145d3e93774120fa8a08679cb63c05ca
Toine Hartman <toine.hartman@swat.engineering>
2025-09-18 09:20:13 +0200

    Refine script.

--- package-and-run.sh
+++ package-and-run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
+set -x
 
 ./package.sh
 
 TMPDIR=$(mktemp --directory)
+# shellcheck disable=SC2012
 VSIX_LATEST=$(ls -t1 **/*.vsix | head -1)
-CODE_COMMAND="code --extensions-dir ${TMPDIR}/extensions --user-data-dir ${TMPDIR}/data"
+CODE_COMMAND="code --extensions-dir $TMPDIR/ext --user-data-dir $TMPDIR/data"
 
-${CODE_COMMAND} --install-extension "${VSIX_LATEST}" --force
-${CODE_COMMAND} --new-window "$1"
+mkdir -p "$TMPDIR"/{ext,data}
+
+$CODE_COMMAND --install-extension "$VSIX_LATEST" --force
+$CODE_COMMAND --new-window "${1:-}" # optionally pass a workspace/folder/file to open
