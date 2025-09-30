388fe9e79048f3f4ff149d4264fa371f6be5aaf7
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-09-30 11:31:58 +0200

    added -l option to activate linting on typescript and java

--- build.sh
+++ build.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 extra_flags=''
+lint=0
 
 clean="clean"
-while getopts 'fd' flag; do
+while getopts 'lfd' flag; do
   case "${flag}" in
     f) extra_flags='-Drascal.compile.skip -Drascal.tutor.skip -DskipTests' ;;
+    l) lint=1 ;;
     d) clean='' ;;
     *) printf "incorrect param, valid params:
     Use -f to skip rascal-compile and tests
-    Use -d to skip cleaning the target folder"
+    Use -d to skip cleaning the target folder
+    Use -l to skip linting
+
+"
         exit 1 ;;
   esac
 done
 
 rm -f rascal-lsp/target/*.jar
 
-(cd rascal-lsp && mvn -B checkstyle:checkstyle  checkstyle:check )
+if (( $lint == 1 )); then
+   (cd rascal-lsp && mvn -B checkstyle:checkstyle  checkstyle:check )
+fi
 (cd rascal-lsp && mvn $clean package -Drascal.monitor.batch $extra_flags )
-(cd rascal-vscode-extension && npm run lint )
+if (( $lint == 1 )); then
+   (cd rascal-vscode-extension && npm run lint )
+fi
 (cd rascal-vscode-extension && npm run lsp4j:package )
 
