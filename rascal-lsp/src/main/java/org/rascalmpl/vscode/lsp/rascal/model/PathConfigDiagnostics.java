e62ae4c63838a49e4c591e63aa27977ae4db5979
Pieter Olivier <pieter.olivier@swat.engineering>
2025-10-02 11:12:00 +0200

    Fixed bookkeeping on which messages to keep and which to discard

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/model/PathConfigDiagnostics.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/model/PathConfigDiagnostics.java
@@ -29,8 +29,12 @@ package org.rascalmpl.vscode.lsp.rascal.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.eclipse.lsp4j.Diagnostic;
@@ -46,36 +50,52 @@ import io.usethesource.vallang.ISourceLocation;
     private final LanguageClient client;
     private final ColumnMaps cm;
     private final Map<ISourceLocation, Map<ISourceLocation, List<Diagnostic>>> publishedProjectDiagsPerFile = new HashMap<>();
+    private final Map<ISourceLocation, Set<ISourceLocation>> filesWithDiagsPerProject = new HashMap<>();
 
     /*package*/ PathConfigDiagnostics(LanguageClient client, ColumnMaps cm) {
         this.client = client;
         this.cm = cm;
     }
 
-    public void publishDiagnostics(ISourceLocation projectRoot, IList messages) {
+    public void publishDiagnostics(ISourceLocation project, IList messages) {
         // Gather messages per file
         Map<ISourceLocation, List<Diagnostic>> diagnosticsPerFile = Diagnostics.translateMessages(messages, cm);
 
         // Publish diagnostics, but only for files for which diagnostics have changed
         for (var entry : diagnosticsPerFile.entrySet()) {
-            ISourceLocation file = entry.getKey();
+            ISourceLocation file = entry.getKey().top();
             List<Diagnostic> newDiagnostics = entry.getValue();
 
-            Map<ISourceLocation, List<Diagnostic>> fileDiagsPerProject = publishedProjectDiagsPerFile.computeIfAbsent(file, (f) -> new HashMap<>());
+            Map<ISourceLocation, List<Diagnostic>> fileDiagsPerProject = publishedProjectDiagsPerFile.computeIfAbsent(file, (f) -> new LinkedHashMap<>());
             @Nullable List<Diagnostic> publishedDiags = fileDiagsPerProject.get(file);
 
             // Only publish diagnostics for a file if the diagnostics for that file have changed in the current project
             if (!newDiagnostics.equals(publishedDiags)) {
                 // The diagnostics for this file related to the current project have changed.
-                // We need to re-publish all diagnostics for this file from all projects
-                fileDiagsPerProject.put(file, newDiagnostics);
-                List<Diagnostic> fileDiagnostics = new ArrayList<>();
-                for (Map<ISourceLocation, List<Diagnostic>> diagsPerFile : publishedProjectDiagsPerFile.values()) {
-                    fileDiagnostics.addAll(diagsPerFile.getOrDefault(file, Collections.emptyList()));
-                }
-
-                client.publishDiagnostics(new PublishDiagnosticsParams(file.getURI().toString(), fileDiagnostics));
+                // We need to re-publish all diagnostics for this file (including the ones from other projects)
+                fileDiagsPerProject.put(project, newDiagnostics);
+                publishFileDiagnostics(file);
             }
         }
+
+        // Now we have published the diagnostics of all files that occur in the list of new messages
+        // Time to remove diagnostics from files that our project previously published diagnostics for but no longer have diagnostics
+        Set<ISourceLocation> newFiles = diagnosticsPerFile.keySet().stream().map(file -> file.top()).collect(Collectors.toSet());
+        Set<ISourceLocation> filesWithoutDiags = new HashSet<>(filesWithDiagsPerProject.getOrDefault(project, Collections.emptySet()));
+        filesWithoutDiags.removeAll(newFiles);
+
+        for (ISourceLocation file : filesWithoutDiags) {
+            publishedProjectDiagsPerFile.get(file).remove(project); // File no longer has diagnostics associated with it in our project
+            publishFileDiagnostics(file);
+        }
+        filesWithDiagsPerProject.put(project, newFiles);
+    }
+
+    private void publishFileDiagnostics(ISourceLocation file) {
+        List<Diagnostic> fileDiagnostics = new ArrayList<>();
+        for (List<Diagnostic> diags :  publishedProjectDiagsPerFile.getOrDefault(file, Collections.emptyMap()).values()) {
+            fileDiagnostics.addAll(diags);
+        }
+        client.publishDiagnostics(new PublishDiagnosticsParams(file.getURI().toString(), fileDiagnostics));
     }
 }
