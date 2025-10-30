2f39c618cb7402aadc887f6db478c622541b94aa
Rodin Aarssen <rodin.aarssen@swat.engineering>
2025-10-30 13:46:59 +0100

    Decimated LSPTerminalREPL again since RascalInterpreterREPL now has a main function

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/terminal/LSPTerminalREPL.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/terminal/LSPTerminalREPL.java
@@ -27,20 +27,8 @@
 package org.rascalmpl.vscode.lsp.terminal;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.OSUtils;
-import org.rascalmpl.debug.IRascalMonitor;
-import org.rascalmpl.ideservices.IDEServices;
-import org.rascalmpl.repl.BaseREPL;
 import org.rascalmpl.repl.rascal.RascalInterpreterREPL;
-import org.rascalmpl.repl.rascal.RascalReplServices;
 import org.rascalmpl.vscode.lsp.uri.jsonrpc.impl.VSCodeVFSClient;
 
 /**
@@ -49,75 +37,24 @@ import org.rascalmpl.vscode.lsp.uri.jsonrpc.impl.VSCodeVFSClient;
  * provide IDE feature to the user of a terminal instance.
  */
 public class LSPTerminalREPL extends RascalInterpreterREPL {
-    private final int ideServicePort;
-
     private LSPTerminalREPL(int ideServicesPort) {
-        this.ideServicePort = ideServicesPort;
+        super(ideServicesPort);
     }
 
-    @Override
-    protected IDEServices buildIDEService(PrintWriter err, IRascalMonitor monitor, Terminal term) {
-        try {
-            return new TerminalIDEClient(ideServicePort, err, monitor, term);
-        } catch (IOException e) {
-            throw new IllegalStateException("Could not build IDE service for REPL", e);
-        }
-    }
-
-    @SuppressWarnings("java:S899") // it's fine to ignore the result of createNewFile
-    private static Path getHistoryFile() throws IOException {
-        var home = Paths.get(System.getProperty("user.home"));
-        var rascal = home.resolve(".rascal");
-
-        if (!Files.exists(rascal)) {
-            Files.createDirectories(rascal);
-        }
-
-        return rascal.resolve(".repl-history-rascal-terminal-jline3");
-    }
-
-
-
-    public static void main(String[] args) throws IOException {
-        int ideServicesPort = -1;
+    public static void main(String[] args) throws IOException, InterruptedException {
         int vfsPort = -1;
 
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("--ideServicesPort")) {
-                ideServicesPort = Integer.parseInt(args[++i]);
-            }
-            else if (args[i].equals("--vfsPort")) {
+            if (args[i].equals("--vfsPort")) {
                 vfsPort = Integer.parseInt(args[++i]);
             }
-        }
-
-        if (ideServicesPort == -1) {
-            throw new IllegalArgumentException("missing --ideServicesPort commandline parameter");
         }
 
         if (vfsPort != -1) {
             VSCodeVFSClient.buildAndRegister(vfsPort);
         }
 
-        var terminalBuilder = TerminalBuilder.builder()
-            .dumb(true) // enable fallback
-            .system(true);
-
-        if (OSUtils.IS_WINDOWS) {
-            terminalBuilder.encoding(StandardCharsets.UTF_8);
-        }
-
-        try {
-            var repl = new BaseREPL(new RascalReplServices(new LSPTerminalREPL(ideServicesPort), getHistoryFile()), terminalBuilder.build());
-            repl.run();
-            System.exit(0); // kill the other threads
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Rascal terminal terminated exceptionally; press any key to exit process.");
-            System.in.read();
-            System.exit(1);
-        }
+        RascalInterpreterREPL.main(args);
     }
 }
 
