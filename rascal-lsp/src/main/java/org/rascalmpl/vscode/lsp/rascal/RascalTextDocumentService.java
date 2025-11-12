2e779b38992f220fd0046f4e6408d66481b75f3f
Toine Hartman <toine.hartman@swat.engineering>
2025-11-12 14:57:16 +0100

    Add labels to workspace edits.

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/RascalTextDocumentService.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/RascalTextDocumentService.java
@@ -506,7 +506,7 @@ public class RascalTextDocumentService implements IBaseTextDocumentService, Lang
                 }
 
                 var changes = DocumentChanges.translateDocumentChanges(this, edits);
-                availableClient().applyEdit(new ApplyWorkspaceEditParams(changes)).thenAccept(editResponse -> {
+                availableClient().applyEdit(new ApplyWorkspaceEditParams(changes, "Rename files")).thenAccept(editResponse -> {
                     if (!editResponse.isApplied()) {
                         throw new RuntimeException("Applying module rename failed" + (editResponse.getFailureReason() != null ? (": " + editResponse.getFailureReason()) : ""));
                     }
