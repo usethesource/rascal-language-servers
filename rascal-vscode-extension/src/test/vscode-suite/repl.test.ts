b476b7356d5ac79ae1a3ffdb484cec7f7aea9f80
Toine Hartman <toine.hartman@swat.engineering>
2025-11-24 11:12:45 +0100

    Always capture all Rascal channels.
    (cherry picked from commit f9197d2036cd226f8c6b4c4a4a2af211b7fb0146)

--- rascal-vscode-extension/src/test/vscode-suite/repl.test.ts
+++ rascal-vscode-extension/src/test/vscode-suite/repl.test.ts
@@ -37,7 +37,7 @@ describe('REPL', function () {
 
     this.timeout(2 * Delays.extremelySlow);
 
-    printRascalOutputOnFailure('Rascal MPL', () => driver, () => ide);
+    printRascalOutputOnFailure(() => driver, () => ide);
 
     before(async () => {
         browser = VSBrowser.instance;
