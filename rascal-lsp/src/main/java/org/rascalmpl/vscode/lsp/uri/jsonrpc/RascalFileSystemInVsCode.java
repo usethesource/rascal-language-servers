5c5f56077ac956b0ff576d3b37ba4752946d6130
Rodin Aarssen <rodin.aarssen@swat.engineering>
2026-03-18 14:50:34 +0100

    Implemented VS Code-specific exception handling and logic in RascalFileSystemInVSCode

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/uri/jsonrpc/RascalFileSystemInVsCode.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/uri/jsonrpc/RascalFileSystemInVsCode.java
@@ -26,10 +26,18 @@
  */
 package org.rascalmpl.vscode.lsp.uri.jsonrpc;
 
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.NotDirectoryException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.eclipse.lsp4j.jsonrpc.ResponseErrorException;
+import org.eclipse.lsp4j.jsonrpc.messages.ResponseError;
 import org.rascalmpl.uri.FileAttributes;
 import org.rascalmpl.uri.UnsupportedSchemeException;
 import org.rascalmpl.uri.remote.IRascalFileSystemServices;
@@ -43,14 +51,14 @@ public class RascalFileSystemInVsCode implements IRascalFileSystemServices {
     @Override
     public CompletableFuture<ISourceLocation> resolveLocation(ISourceLocation loc) {
         logger.trace("resolveLocation: {}", loc);
-        return IRascalFileSystemServices.super.resolveLocation(Locations.toClientLocation(loc));
+        return IRascalFileSystemServices.super.resolveLocation(Locations.toClientLocation(loc)).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<Void> watch(WatchRequest params) {
         logger.trace("watch: {}", params.getLocation());
         if (Locations.isWrappedOpaque(params.getLocation())) {
-            throw new VSCodeFSError(new UnsupportedSchemeException("Opaque locations are not supported by Rascal"));
+            throw new ResponseErrorException(translate(new UnsupportedSchemeException("Opaque locations are not supported by Rascal")));
         }
         return IRascalFileSystemServices.super.watch(params);
     }
@@ -58,47 +66,104 @@ public class RascalFileSystemInVsCode implements IRascalFileSystemServices {
     @Override
     public CompletableFuture<FileAttributes> stat(ISourceLocation loc) {
         logger.trace("stat: {}", loc);
-        return IRascalFileSystemServices.super.stat(Locations.toClientLocation(loc));
+        return IRascalFileSystemServices.super.stat(Locations.toClientLocation(loc)).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<FileWithType[]> list(ISourceLocation loc) {
         logger.trace("list: {}", loc);
-        return IRascalFileSystemServices.super.list(Locations.toClientLocation(loc));
+        return IRascalFileSystemServices.super.list(Locations.toClientLocation(loc)).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<Void> mkDirectory(ISourceLocation loc) {
         logger.trace("mkDirectory: {}", loc);
-        return IRascalFileSystemServices.super.mkDirectory(Locations.toClientLocation(loc));
+        return IRascalFileSystemServices.super.mkDirectory(Locations.toClientLocation(loc)).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<String> readFile(ISourceLocation loc) {
         logger.trace("readFile: {}", loc);
-        return IRascalFileSystemServices.super.readFile(Locations.toClientLocation(loc));
+        return IRascalFileSystemServices.super.readFile(Locations.toClientLocation(loc)).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<Void> writeFile(ISourceLocation loc, String content, boolean append) {
         logger.info("writeFile: {}", loc);
-        return IRascalFileSystemServices.super.writeFile(Locations.toClientLocation(loc), content, append);
+        if (reg.exists(loc) && reg.isDirectory(loc)) {
+            return CompletableFuture.failedFuture(new ResponseErrorException(fileIsADirectory(loc)));
+        }
+        return IRascalFileSystemServices.super.writeFile(Locations.toClientLocation(loc), content, append).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<Void> remove(ISourceLocation loc, boolean recursive) {
         logger.trace("remove: {}", loc);
-        return IRascalFileSystemServices.super.remove(Locations.toClientLocation(loc), recursive);
+        return IRascalFileSystemServices.super.remove(Locations.toClientLocation(loc), recursive).exceptionally(this::handleException);
     }
 
     @Override
     public CompletableFuture<Void> rename(ISourceLocation from, ISourceLocation to, boolean overwrite) {
         logger.trace("rename: {} to {}", from, to);
-        return IRascalFileSystemServices.super.rename(Locations.toClientLocation(from), Locations.toClientLocation(to), overwrite);
+        return IRascalFileSystemServices.super.rename(Locations.toClientLocation(from), Locations.toClientLocation(to), overwrite).exceptionally(this::handleException);
     }
 
     @Override
     public void onDidChangeFile(ISourceLocation loc, int type, String watchId) {
         logger.trace("onDidChangeFile: {}", loc);
+    }
+
+    private static ResponseError fileExists(Object data) {
+        return new ResponseError(-1, "File exists", data);
+    }
+
+    private static ResponseError fileIsADirectory(Object data) {
+        return new ResponseError(-2, "File is a directory", data);
+    }
+
+    private static ResponseError fileNotADirectory(Object data) {
+        return new ResponseError(-3, "File is not a directory", data);
+    }
+    
+    private static ResponseError fileNotFound(Object data) {
+        return new ResponseError(-4, "File is not found", data);
+    }
+    
+    private static ResponseError noPermissions(Object data) {
+        return new ResponseError(-5, "No permissions", data);
+    }
+
+    @SuppressWarnings("unused")
+    private static ResponseError unavailable(Object data) {
+        return new ResponseError(-6, "Unavailable", data);
+    }
+
+    private static ResponseError generic(@Nullable String message, Object data) {
+        return new ResponseError(-99, message == null ? "no error message was provided" : message, data);
+    }
+
+    public static ResponseError translate(Throwable original) {
+        if (original instanceof CompletionException) {
+            var cause = original.getCause();
+            
+            if (cause instanceof FileNotFoundException || cause instanceof UnsupportedSchemeException || cause instanceof URISyntaxException) {
+                return fileNotFound(cause);
+            } else if (cause instanceof FileAlreadyExistsException) {
+                return fileExists(cause);
+            } else if (cause instanceof NotDirectoryException) {
+                return fileNotADirectory(cause);
+            } else if (cause instanceof SecurityException) {
+                return noPermissions(cause);
+            } else if (cause instanceof ResponseErrorException) {
+                return ((ResponseErrorException) cause).getResponseError();
+            } else {
+                return generic(cause.getMessage(), original);
+            }
+        }
+        return generic(original.getMessage(), original);
+    }
+
+    private <T, U extends T> T handleException(Throwable t) throws ResponseErrorException {
+        throw new ResponseErrorException(translate(t));
     }
 }
