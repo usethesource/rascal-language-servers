b3b28c2d2b529e06871a76ba3b2d70bfc80cc57c
Toine Hartman <toine.hartman@swat.engineering>
2026-04-15 11:09:15 +0200

    Fix flaky code lens test.

--- rascal-vscode-extension/src/test/vscode-suite/dsl.test.ts
+++ rascal-vscode-extension/src/test/vscode-suite/dsl.test.ts
@@ -222,8 +222,15 @@ end
     it("code lens works", async function() {
         if (errorRecovery) { this.skip(); }
         const editor = await ide.openModule(TestWorkspace.picoFile);
-        const lens = await driver.wait(() => editor.getCodeLens("Rename variables a to b."), Delays.verySlow, "Rename lens should be available");
-        await lens!.click();
+        await driver.wait(async () => {
+            try {
+                const lens = await editor.getCodeLens("Rename variables a to b.");
+                await lens!.click();
+                return true;
+            } catch (e) {
+                return false;
+            }
+        }, Delays.verySlow, "Rename lens should be available");
         await ide.assertLineBecomes(editor, 9, "b := 2;", "a variable should be changed to b");
     });
 
