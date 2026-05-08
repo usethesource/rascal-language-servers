03e601198dfcb05c59ddf274b6b96e06af0803ea
Toine Hartman <toine.hartman@swat.engineering>
2026-05-08 12:21:49 +0200

    Add TODO for later.

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/parametric/routing/RoutingTextDocumentService.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/parametric/routing/RoutingTextDocumentService.java
@@ -259,6 +259,7 @@ public class RoutingTextDocumentService implements IBaseTextDocumentService, Fut
     @Override
     public CompletableFuture<IValue> executeCommand(String languageName, String command) {
         return availableServer().languageByName(languageName)
+            // TODO Go via workspace service instead
             .thenCompose(s -> s.getIBaseTextDocumentService().executeCommand(languageName, command));
     }
 
