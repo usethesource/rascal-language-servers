6c40a51ab130508762120f28deab2f6884a30f8a
Toine Hartman <toine.hartman@swat.engineering>
2026-06-03 12:23:52 +0200

    More robust content check.

--- rascal-vscode-extension/src/test/vscode-suite/utils.ts
+++ rascal-vscode-extension/src/test/vscode-suite/utils.ts
@@ -250,7 +250,7 @@ export class IDEOperations {
 
     assertLineBecomes(editor: TextEditor, lineNumber: number, lineContents: string, msg: string, wait = Delays.verySlow) : Promise<boolean> {
         return this.driver.wait(async () => {
-            const currentContent = (await editor.getTextAtLine(lineNumber)).trim();
+            const currentContent = (await ignoreFails(editor.getTextAtLine(lineNumber)))?.trim();
             return currentContent === lineContents;
         }, wait, msg, 100);
     }
