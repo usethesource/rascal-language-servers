313a6df7731a1e7322829744b12df7d7b459afb9
Toine Hartman <toine.hartman@swat.engineering>
2026-06-08 10:00:18 +0200

    Revert code lens clicking changes.

--- rascal-vscode-extension/src/test/vscode-suite/utils.ts
+++ rascal-vscode-extension/src/test/vscode-suite/utils.ts
@@ -439,24 +439,15 @@ export class IDEOperations {
 
     async clickCodeLens(editor: TextEditor, name: string, timeout = Delays.slow): Promise<void> {
         const lens = await this.driver.wait(async () => {
-            const lenses = await editor.getCodeLenses();
-            console.log("All lenses:");
-            console.log(await Promise.all(lenses.map(l => l.getLocatorInfo())));
-            for (const lens of lenses) {
-                try {
-                    const title = await lens.getText();
-                    const match = title.match(name);
-                    if (match && match.length > 0 && await lens.isDisplayed()) {
-                        return lens;
-                    }
-                } catch (e: unknown) {
-                    continue;
-                }
+            const lens = await editor.getCodeLens(name);
+            if (lens?.isDisplayed()) {
+                console.log(`Found code lens: ${name}`);
+                return lens;
             }
             return undefined;
         }, timeout, `Cannot find code lens '${name}'`);
 
-        await lens!.click();
+        await lens!.safeClick();
     }
 
     statusContains(needle: string): () => Promise<boolean> {
