12206c3b78e85c806280b1400af95e4022ea17f4
Toine Hartman <toine.hartman@swat.engineering>
2026-07-22 09:54:23 +0200

    Remove unused constant from #1101.

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/locations/Locations.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/util/locations/Locations.java
@@ -29,8 +29,6 @@ package org.rascalmpl.vscode.lsp.util.locations;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Set;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -65,8 +63,6 @@ public class Locations {
     // Can only contain alphanumeric characters or "+", "-", ".", and should start with an alpha
     // https://datatracker.ietf.org/doc/html/rfc3986#section-3.1
     private static final String OPAQUE_SCHEME = "opaque-lsp-";
-
-    public static final Set<String> TRANSLATED_SCHEMES = Set.of(OPAQUE_SCHEME, LSPOpenFileResolver.LSP_OPEN_SCHEME);
 
     public static ISourceLocation toClientLocation(ISourceLocation loc) {
         loc = LSPOpenFileResolver.stripLspPrefix(loc);
