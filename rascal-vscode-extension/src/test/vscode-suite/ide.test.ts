11b681dd8687215d4822a3028b330dc1138eb248
Rodin Aarssen <rodin.aarssen@swat.engineering>
2026-07-28 11:57:47 +0200

    Avoid use of setTextAtLine in UI test

--- rascal-vscode-extension/src/test/vscode-suite/ide.test.ts
+++ rascal-vscode-extension/src/test/vscode-suite/ide.test.ts
@@ -301,7 +301,8 @@ describe('IDE', function () {
         // Fix type error to avoid a failing "after each" hook in CI
         const importeeEditor = await ide.openModule(TestWorkspace.importeeFile);
         await ide.openModule(TestWorkspace.importeeFile);
-        await importeeEditor.setTextAtLine(2, "public int foo;");
+        await importeeEditor.setCursor(2, 1);
+        await importeeEditor.typeText("public int foo;");
 
         const explorer = await (await bench.getActivityBar().getViewControl("Explorer"))!.openView();
         const workspace = await explorer.getContent().getSection("test (Workspace)");
