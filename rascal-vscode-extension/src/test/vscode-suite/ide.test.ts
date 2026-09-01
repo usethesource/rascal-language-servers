d8874ec0a5d18f598863e4cb61c2b86b5255509e
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2026-09-01 10:18:10 +0200

    added regression tests for IDE integrated Rascal formatters

--- rascal-vscode-extension/src/test/vscode-suite/ide.test.ts
+++ rascal-vscode-extension/src/test/vscode-suite/ide.test.ts
@@ -291,4 +291,47 @@ describe('IDE', function () {
 
         await ide.checkNoDiagnosticsAnymore();
     });
+
+    it ("full file formatting works", async() => {
+        const editor = await ide.openModule(TestWorkspace.uglyLibFile);
+        await editor.setCursor(1, 1);
+
+        // Before moving, check that Rascal is really loaded
+        const checkRascalStatus = isLanguageLoading(bench, "Rascal");
+        await driver.wait(async () => await ignoreFails(checkRascalStatus()) === false, Delays.extremelySlow, "Rascal evaluators have not finished loading");
+
+        try {
+            await bench.executeCommand("editor.action.formatDocument");
+
+            await driver.wait(() => (editor.isDirty()), Delays.extremelySlow, "Formatter should have resulted in changes in the editor");
+
+            const editorText = await editor.getText();
+            expect(editorText.split("\n").length > 5);
+        }
+        finally {
+            await ide.revertOpenChanges();
+        }
+    });
+
+    it ("selection formatting works", async() => {
+        const editor = await ide.openModule(TestWorkspace.uglyLibFile);
+
+        // Before moving, check that Rascal is really loaded
+        const checkRascalStatus = isLanguageLoading(bench, "Rascal");
+        await driver.wait(async () => await ignoreFails(checkRascalStatus()) === false, Delays.extremelySlow, "Rascal evaluators have not finished loading");
+
+        try {
+            await editor.setCursor(1, 34); // this is on the `if` keyword
+            await bench.executeCommand("editor.action.formatSelection"); // should apply to a focus on the entire if statement
+
+            await driver.wait(() => (editor.isDirty()), Delays.extremelySlow, "Formatter should have resulted in changes in the editor");
+
+            const editorText = await editor.getText();
+            // the rest of the file remains on one line, but the if-then is formatted
+            expect(editorText.split("\n").length > 3);
+        }
+        finally {
+            await ide.revertOpenChanges();
+        }
+    });
 });
