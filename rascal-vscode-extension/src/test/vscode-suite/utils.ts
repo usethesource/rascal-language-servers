f44be1f645fd91f723cf32bb1108754ef2c58fc1
Toine Hartman <toine.hartman@swat.engineering>
2026-09-10 15:59:36 +0200

    Simplify output text condition.

--- rascal-vscode-extension/src/test/vscode-suite/utils.ts
+++ rascal-vscode-extension/src/test/vscode-suite/utils.ts
@@ -549,12 +549,8 @@ export async function getOutput(channel: OutputChannel, driver: WebDriver): Prom
     const output = await bottomBar.openOutputView();
     await output.selectChannel(channel);
     await output.waitForStable();
-    await driver.wait(async () => (await output.getText()).length > 0, Delays.fast, "Output channel should load");
-    const text = await output.getText();
-
-    if (text.trim().length === 0) {
-        fail("No output found!");
-    }
+    // Sometimes, we seem to get the text before everything is loaded in the window. If that happens, we might need an extra condition or sleep here.
+    const text = await driver.wait(() => output.getText(), Delays.fast, "Channel should contain some output");
     await ignoreFails(bottomBar.closePanel());
     return text;
 }
