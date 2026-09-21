01aafa018c9a8fa69463c1184a027f0ad8d74ace
Rodin Aarssen <rodin.aarssen@swat.engineering>
2026-09-21 15:10:30 +0200

    Extracted code action conversion function

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/conversion/CodeActions.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/rascal/conversion/CodeActions.java
@@ -87,13 +87,16 @@ public class CodeActions {
 
     /* merges two streams of CodeAction terms and then converts them to LSP objects */
     public static CompletableFuture<List<Either<Command, CodeAction>>> mergeAndConvertCodeActions(IBaseTextDocumentService doc, String dedicatedLanguageName, String languageName, CompletableFuture<Stream<IValue>> quickfixes, CompletableFuture<Stream<IValue>> codeActions) {
-        return codeActions.thenCombine(quickfixes, (actions, quicks) ->
-            Stream.concat(quicks, actions)
-                .map(IConstructor.class::cast)
-                .map(cons -> constructorToCodeAction(doc, dedicatedLanguageName, languageName, cons))
-                .map(Either::<Command,CodeAction>forRight)
-                .collect(Collectors.toList())
-        );
+        return convertCodeActions(doc, dedicatedLanguageName, languageName, codeActions.thenCombine(quickfixes, (actions, quicks) -> Stream.concat(quicks, actions)));
+    }
+
+    public static CompletableFuture<List<Either<Command, CodeAction>>> convertCodeActions(IBaseTextDocumentService doc, String dedicatedLanguageName, String languageName, CompletableFuture<Stream<IValue>> items) {
+        return items.thenApply(is -> is
+            .map(IConstructor.class::cast)
+            .map(cons -> constructorToCodeAction(doc, dedicatedLanguageName, languageName, cons))
+            .map(Either::<Command,CodeAction>forRight)
+            .collect(Collectors.toList()))
+            ;
     }
 
     private static CodeAction constructorToCodeAction(IBaseTextDocumentService doc, String dedicatedLanguageName, String languageName, IConstructor codeAction) {
