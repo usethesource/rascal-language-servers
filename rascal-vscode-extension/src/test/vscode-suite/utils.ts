babd547ce53be9c0ca35b51dbbbcdecf3d9e641b
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2026-09-25 15:44:50 +0200

    Try fix UI test

--- rascal-vscode-extension/src/test/vscode-suite/utils.ts
+++ rascal-vscode-extension/src/test/vscode-suite/utils.ts
@@ -127,7 +127,7 @@ export class RascalREPL {
                     if (this.ide) {
                         await this.ide.screenshot("waitForReplReady");
                     }
-                    console.log(`terminal: ${this.terminal}, terminal name: ${console.log(await this.terminal.getCurrentChannel())}, output:\n\`\`\`\n${output}\n\`\`\``);
+                    console.log(`terminal: ${this.terminal}, terminal name: "${await this.terminal.getCurrentChannel()}", output:\n\`\`\`\n${output}\n\`\`\``);
                     if (/rascal>\s*$/.test(output)) {
                         stopRunning = true;
                         return true;
@@ -165,6 +165,8 @@ export class RascalREPL {
     async connect() {
         this.terminal = (await this.driver.wait(() => ignoreFails(new TerminalView().wait(100)), Delays.verySlow, "Waiting to find terminal view"))!;
         console.log(`this.terminal:\n\`\`\`\n${JSON.stringify(this.terminal)}\n\`\`\``);
+        console.log(`getChannelNames(): ${await this.terminal.getChannelNames()}`);
+        await this.terminal.selectChannel("Rascal terminal");
         let currentChannel: string | undefined = '';
         const found = await this.driver.wait(async () => (currentChannel = (await ignoreFails(this.terminal.getCurrentChannel())))?.includes("Rascal"),
             Delays.slow, "Rascal REPL should be opened");
